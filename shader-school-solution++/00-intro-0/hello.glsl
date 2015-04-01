bc7ad5dc9757a505ef2483bd76d5d6fd127d32c7
utensil <utensilcandel@gmail.com>
2015-04-01 18:06:37 +0800

    Remove accidentally commited directory

--- shader-school-solution++/00-intro-0/hello.glsl
+++
@@ -1,6 +0,0 @@
-float brightness() {
-  return 1.0;
-}
-
-//Do not change this line
-#pragma glslify: export(brightness)
--- shader-school-solution++/01-intro-1/hello.glsl
+++
@@ -1,7 +0,0 @@
-highp float sum(highp float x, highp float y) {
-
-  return x + y;
-}
-
-//Do not change this line
-#pragma glslify: export(sum)
