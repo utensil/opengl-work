6162a61916a0b6f4b0baea16d8536d1ae2ad8cc2
utensil <utensilcandel@gmail.com>
2015-04-08 23:27:38 +0800

    Solve 17: flat light

--- shader-school-solutions/17-light-1/fragment.glsl
+++ shader-school-solutions/17-light-1/fragment.glsl
@@ -4,5 +4,5 @@ uniform mat4 model, view, projection;
 uniform vec3 ambient;
 
 void main() {
-  gl_FragColor = vec4(1,1,1,1);
+  gl_FragColor = vec4(ambient, 1);
 }
--- shader-school-solutions/17-light-1/vertex.glsl
+++ shader-school-solutions/17-light-1/vertex.glsl
@@ -5,5 +5,5 @@ uniform mat4 model, view, projection;
 uniform vec3 ambient;
 
 void main() {
-  gl_Position = vec4(position, 1);
+  gl_Position = projection * view * model * vec4(position, 1);
 }
